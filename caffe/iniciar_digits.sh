85591ad9385aa204fc5673b6bc1dcc35906e2a72
Diego <sandiego206@gmail.com>
2015-07-16 16:23:26 -0500

    Commit inicial

---
+++ caffe/iniciar_digits.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/sandiego/Programas/digits/digits-devserver
---
+++ caffe/slides_caffe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ipython nbconvert Tutorial_Caffe.ipynb --to slides --post serve --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.5.0"
+
